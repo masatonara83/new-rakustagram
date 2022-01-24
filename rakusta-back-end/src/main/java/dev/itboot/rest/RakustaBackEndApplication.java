f67531f6d59bb5b0bfb205625d5c0a0375748f91
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-01-24 11:42:46 +0900

    firstcommit

---
+++ rakusta-back-end/src/main/java/dev/itboot/rest/RakustaBackEndApplication.java
@@ -0,0 +1,13 @@
+package dev.itboot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakustaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakustaBackEndApplication.class, args);
+	}
+
+}
---
+++ rakusta-back-end/src/test/java/dev/itboot/rest/RakustaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.itboot.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakustaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
