b8a65b674ad0cedf03af975b468fb24a81dff82d
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-01-24 12:18:14 +0900

    ドメイン作成

---
+++ rakusta-back-end/src/main/java/dev/itboot/rest/model/Comment.java
@@ -0,0 +1,62 @@
+package dev.itboot.rest.model;
+
+import java.sql.Timestamp;
+
+public class Comment {
+
+	private Integer commentId;
+	private Integer userId;
+	private Integer articleId;
+	private String comment;
+	private Timestamp commentPostDate;
+	private User user;
+	
+	//getter and setter
+	public Integer getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(Integer commentId) {
+		this.commentId = commentId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getArticleId() {
+		return articleId;
+	}
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Timestamp getCommentPostDate() {
+		return commentPostDate;
+	}
+	public void setCommentPostDate(Timestamp commentPostDate) {
+		this.commentPostDate = commentPostDate;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	@Override
+	public String toString() {
+		return "Comment [commentId=" + commentId + ", userId=" + userId + ", articleId=" + articleId + ", comment="
+				+ comment + ", commentPostDate=" + commentPostDate + ", user=" + user + "]";
+	}
+	
+	
+	
+	
+}
+
---
+++ rakusta-back-end/src/main/java/dev/itboot/rest/model/User.java
@@ -0,0 +1,114 @@
+package dev.itboot.rest.model;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * usersテーブル
+ * @author naramasato
+ *
+ */
+public class User {
+
+	//ユーザーID
+	private 	Integer 		userId;
+	//ユーザーネーム
+	private 	String 			userName;	
+	//ユーザーの本名
+	private 	String 			userFullName;
+	//ユーザー概要
+	private 	String 			userOverview;
+	//メールアドレス
+	private 	String 			userMailAddress;
+	//パスワード
+	private 	String 			userPassword;
+	
+	//登録日	
+	private 	Timestamp 		userRegistrationDate;
+	//更新日
+	private 	Date 			updateDate;
+	//
+	private		String			userImagePath;
+	//FollowをListで格納
+	private 	List<Follow> 	followList;
+	//ArticleをListで格納
+	private 	List<Article> 	articleList;
+	
+	//ゲッターとセッター
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserFullName() {
+		return userFullName;
+	}
+	public void setUserFullName(String userFullName) {
+		this.userFullName = userFullName;
+	}
+	public String getUserOverview() {
+		return userOverview;
+	}
+	public void setUserOverview(String userOverview) {
+		this.userOverview = userOverview;
+	}
+	public String getUserMailAddress() {
+		return userMailAddress;
+	}
+	public void setUserMailAddress(String userMailAddress) {
+		this.userMailAddress = userMailAddress;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public Timestamp getUserRegistrationDate() {
+		return userRegistrationDate;
+	}
+	public void setUserRegistrationDate(Timestamp userRegistrationDate) {
+		this.userRegistrationDate = userRegistrationDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	public String getUserImagePath() {
+		return userImagePath;
+	}
+	public void setUserImagePath(String userImagePath) {
+		this.userImagePath = userImagePath;
+	}
+	public List<Follow> getFollowList() {
+		return followList;
+	}
+	public void setFollowList(List<Follow> followList) {
+		this.followList = followList;
+	}
+	public List<Article> getArticleList() {
+		return articleList;
+	}
+	public void setArticleList(List<Article> articleList) {
+		this.articleList = articleList;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userFullName=" + userFullName
+				+ ", userOverview=" + userOverview + ", userMailAddress=" + userMailAddress + ", userPassword="
+				+ userPassword + ", userRegistrationDate=" + userRegistrationDate + ", updateDate=" + updateDate
+				+ ", userImagePath=" + userImagePath + ", followList=" + followList + ", articleList=" + articleList
+				+ "]";
+	}
+	
+}
